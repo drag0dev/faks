a6830228870b28becf5fa8a6f81e081e9e0bce82
Dragoslav Tamindzija <dragodev@protonmail.com>
2021-11-27 10:50:30 +0100

    zavrseni svi

--- cas_11_vezbe/zadatak_6_7.py
+++ cas_11_vezbe/zadatak_6_7.py
@@ -1,5 +1,5 @@
 def uskrs(godina):
-    if godina< 1982 or godina > 2048:
+    if godina< 1900 or godina > 2099:
         print('Godina nije u opsegu 1982-2048!')
         return
     
@@ -10,10 +10,14 @@ def uskrs(godina):
     e = (2*b+4*c+6*d+5)%7
 
     datum = 22 + d + e
+
+    if godina in [1954, 1981, 2049, 2076]:
+        datum -= 7
+
     if(datum>31):
         print('Uskrs je', str(datum-31) + '. aprila', str(godina) + '. godine.')
     else:
         print('Uskrs je', str(datum) + '. marta', str(godina) + '. godine.')
 
 if __name__ == '__main__':
-    uskrs(1988)
+    uskrs(2049)
